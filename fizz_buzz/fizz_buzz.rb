abc39caf607e01f7bf2f918a3894791b441fa914
nawwab <nawwabzia@gmail.com>
2023-05-16 10:22:55 +0700

    fizz_buzz: add cli api
    - no tdd involved

--- fizz_buzz/fizz_buzz.rb
+++ fizz_buzz/fizz_buzz.rb
@@ -1,4 +1,37 @@
+require 'optparse'
+
 class FizzBuzz
+  def self.start(args)
+    options = {}
+    parser = OptionParser.new do |parser|
+      parser.banner = "Usage: #{parser.program_name} [options] limit"
+      parser.on("-f", "--fizz NUMBER", Integer, "Positive NUMBER as divider for fizz") do |number|
+        options[:fizz_divider] = number
+      end
+      parser.on("-b", "--buzz NUMBER", Integer, "Positive NUMBER as divider for buzz") do |number|
+        options[:buzz_divider] = number
+      end
+    end
+
+    begin
+      parser.parse!(args)
+      options[:until] = args.pop.to_i
+
+      if options[:fizz_divider].nil?
+        raise OptionParser::MissingArgument, '--fizz'
+      elsif options[:buzz_divider].nil?
+        raise OptionParser::MissingArgument, '--buzz'
+      elsif options[:until].nil? || options[:until] == 0
+        raise OptionParser::MissingArgument, 'limit number not provided'
+      end
+
+      puts new(options[:fizz_divider], options[:buzz_divider]).count(options[:until])
+    rescue => e
+      puts e
+      puts parser
+    end
+  end
+
   def initialize(fizz_divider, buzz_divider)
     @fizz_divider = fizz_divider
     @buzz_divider = buzz_divider
